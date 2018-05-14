01a78efa79b52351892c97140c466bbdbc6c97e1
dahby <dstoll243@gmail.com>
2018-05-14 11:11:42 -0700

    server file built

---
+++ src/lib/server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import express from 'express';
+import mongoose from 'mongoose';
+import logger from './logger';
+import errorMiddleware from './error-middleware';
+
+const app = express();
+let server = null;
+
+// routes will be app.use'd here
+
+app.all('*', (request, response) => {
+  logger.log(logger.INFO, 'Returning a 404 from the catch-all/default route');
+  return response.sendStatus(404);
+});
+
+app.use(errorMiddleware);
+
+const startServer = () => {
+  return mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+      server = app.listen(process.env.PORT, () => {
+        logger.log(logger.INFO, `Server is listening on port ${process.env.PORT}`);
+      });
+    });
+};
+
+const stopServer = () => {
+  return mongoose.disconnect()
+    .then(() => {
+      server.close(() => {
+        logger.log(logger.INFO, 'Server is off');
+      });
+    });
+};
+
+export { startServer, stopServer };
