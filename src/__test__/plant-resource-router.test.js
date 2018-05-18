5327bf5f0e87180fe54236b5676404de40cc5a80
danielvportlandia <danielshelton90@gmail.com>
2018-05-18 07:26:42 -0700

    cleaning up comments.

--- src/__test__/plant-resource-router.test.js
+++ src/__test__/plant-resource-router.test.js
@@ -16,14 +16,14 @@ describe('PLANT-RESOURCE SCHEMA', () => {
   afterEach(removePlantMock);
   afterEach(removePlantResourceMock);
   afterAll(stopServer);
+  jest.setTimeout(20000);
 
-  describe('POST /entry', () => {
-    jest.setTimeout(20000);
+  describe('POST /bloomiogarden', () => {
     test('POST - should return a 200 status code for a successful post.', () => {
       return createAdminMock()
         .then((responseMock) => {
           const { token } = responseMock.accountSetMock;
-          return superagent.post(`${apiURL}/entry`)
+          return superagent.post(`${apiURL}/bloomiogarden`)
             .set('Authorization', `Bearer ${token}`)
             .send({
               commonName: 'Chinese Money Plant',
@@ -49,7 +49,7 @@ describe('PLANT-RESOURCE SCHEMA', () => {
       return createAdminMock()
         .then((responseMock) => {
           const { token } = responseMock.accountSetMock;
-          return superagent.post(`${apiURL}/entry`)
+          return superagent.post(`${apiURL}/bloomiogarden`)
             .set('Authorization', `Bearer ${token}`)
             .send({
               commonName: 'Bromeliad',
@@ -63,7 +63,7 @@ describe('PLANT-RESOURCE SCHEMA', () => {
     test('POST - should return a 401 status code when no token given.', () => {
       return createAdminMock()
         .then(() => {
-          return superagent.post(`${apiURL}/entry`)
+          return superagent.post(`${apiURL}/bloomiogarden`)
             .set('Authorization', 'Bearer badToken')
             .send({
               commonName: 'Coffee Plant',
@@ -83,7 +83,7 @@ describe('PLANT-RESOURCE SCHEMA', () => {
       return createProfileMock()
         .then((responseMock) => {
           const { token } = responseMock.accountSetMock;
-          return superagent.post(`${apiURL}/entry`)
+          return superagent.post(`${apiURL}/bloomiogarden`)
             .set('Authorization', `Bearer ${token}`)
             .send({
               commonName: 'Bird of Paradise',
@@ -101,13 +101,13 @@ describe('PLANT-RESOURCE SCHEMA', () => {
     });
   });                               
 
-  describe('GET /entry/:id', () => {
+  describe('GET /bloomiogarden/:id', () => {
     test('GET - should return a 200 status code and the specified plant template.', () => {
       let plantTemplateTest = null;
       return createPlantResourceMock()
         .then((plantTemplate) => {
           plantTemplateTest = plantTemplate;
-          return superagent.get(`${apiURL}/entry/${plantTemplateTest.plantResource._id}`);
+          return superagent.get(`${apiURL}/bloomiogarden/${plantTemplateTest.plantResource._id}`);
         /* .set('Authorization', `Bearer ${plantTemplateTest.accountSetMock.token}`); */
         })
         .then((response) => {
@@ -117,7 +117,7 @@ describe('PLANT-RESOURCE SCHEMA', () => {
     test('GET - should return a 400 status code for no id.', () => {
       return createPlantResourceMock()
         .then(() => {
-          return superagent.post(`${apiURL}/entry/`);
+          return superagent.post(`${apiURL}/bloomiogarden/`);
         })
         .then(Promise.reject)
         .catch((err) => {
@@ -127,10 +127,209 @@ describe('PLANT-RESOURCE SCHEMA', () => {
     test('GET - should return a 404 status code for bad id.', () => {
       return createPlantResourceMock()
         .then(() => {
-          return superagent.post(`${apiURL}/entry/badId`);
+          return superagent.post(`${apiURL}/bloomiogarden/badId`);
         })
         .catch((err) => {
           expect(err.status).toEqual(404);
+        });
+    });
+  });
+
+  describe('PUT /bloomiogarden', () => {
+    test('PUT - should return a 200 status code and the updated plant-resource.', () => {
+      const resultMock = {};
+      return createAdminMock()
+        .then((responseMock) => {
+          const { token } = responseMock.accountSetMock;
+          resultMock.token = token;
+          return superagent.post(`${apiURL}/bloomiogarden`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              commonName: 'Tomato',
+              scientificName: 'Solanum Iycopersicum',
+              groupType: 'vegetable',
+              waterDate: 1,
+              fertilizerDate: 7,
+              mistingDate: 10,
+            })
+            .then((postedPlantResource) => {
+              resultMock.postedPlantResource = postedPlantResource.body;
+              return superagent.put(`${apiURL}/bloomiogarden/${postedPlantResource.body._id}`)
+                .set('Authorization', `Bearer ${resultMock.token}`)
+                .send({
+                  commonName: 'testTomato',
+                })
+                .then((response) => {
+                  expect(response.status).toEqual(200);
+                  expect(response.body.commonName).toEqual('testTomato');
+                });
+            });
+        });
+    });
+    test('PUT - should return a 401 status code if user did not have admin access.', () => {
+      const resultMock = {};
+      return createAdminMock()
+        .then((responseMock) => {
+          const { token } = responseMock.accountSetMock;
+          resultMock.token = token;
+          return superagent.post(`${apiURL}/bloomiogarden`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              commonName: 'Bush Violet',
+              scientificName: 'Browalia',
+              groupType: 'shrub',
+              waterDate: 1,
+              fertilizerDate: 7,
+              mistingDate: 4,
+            })
+            .then((postedPlantResource) => {
+              resultMock.postedPlantResource = postedPlantResource.body;
+              return createProfileMock()
+                .then((secondResponseMock) => {
+                  const { userToken } = secondResponseMock.accountSetMock;
+                  return superagent.put(`${apiURL}/bloomiogarden/${resultMock.postedPlantResource._id}`)
+                    .set('Authorization', `Bearer ${userToken}`)
+                    .send({
+                      waterDate: 'every day',
+                    })
+                    .then(Promise.reject)
+                    .catch((error) => {
+                      expect(error.status).toEqual(401);
+                    });
+                });
+            });
+        });
+    });
+    test('PUT - should return a 404 status code for a bad address.', () => {
+      const resultMock = {};
+      return createAdminMock()
+        .then((responseMock) => {
+          const { token } = responseMock.accountSetMock;
+          resultMock.token = token;
+          return superagent.post(`${apiURL}/bloomiogarden`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              commonName: 'Cat Grass',
+              scientificName: 'Cyperus involucratus Zumula',
+              groupType: 'grass',
+              waterDate: 1,
+              fertilizerDate: 7,
+              mistingDate: 4,
+            })
+            .then((postedPlantResource) => {
+              resultMock.postedPlantResource = postedPlantResource.body;
+              return superagent.put(`${apiURL}/bloomiogarden/${postedPlantResource.body._id}badaddress`)
+                .set('Authorization', `Bearer ${resultMock.token}`)
+                .send({
+                  commonName: 'kitty grass',
+                })
+                .then(Promise.reject)
+                .catch((error) => {
+                  expect(error.status).toEqual(404);
+                });
+            });
+        });
+    });
+    test('PUT - should return a 500 status code if reassigned property was the wrong type.', () => {
+      const resultMock = {};
+      return createAdminMock()
+        .then((responseMock) => {
+          const { token } = responseMock.accountSetMock;
+          resultMock.token = token;
+          return superagent.post(`${apiURL}/bloomiogarden`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              commonName: 'Bush Violet',
+              scientificName: 'Browalia',
+              groupType: 'shrub',
+              waterDate: 1,
+              fertilizerDate: 7,
+              mistingDate: 4,
+            })
+            .then((postedPlantResource) => {
+              resultMock.postedPlantResource = postedPlantResource.body;
+              return superagent.put(`${apiURL}/bloomiogarden/${postedPlantResource.body._id}`)
+                .set('Authorization', `Bearer ${resultMock.token}`)
+                .send({
+                  waterDate: 'every day',
+                })
+                .then(Promise.reject)
+                .catch((error) => {
+                  expect(error.status).toEqual(500);
+                });
+            });
+        });
+    });
+  });
+
+  describe('DELETE /bloomiogarden/:id', () => {
+    test('DELETE - should return a 204 status code if plant-resource successfully deleted.', () => {
+      const resultMock = {};
+      return createAdminMock()
+        .then((responseMock) => {
+          const { token } = responseMock.accountSetMock;
+          resultMock.token = token;
+          return superagent.post(`${apiURL}/bloomiogarden`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              commonName: 'Chinese Evergreen',
+              scientificName: 'Aglaonema pictum',
+              groupType: 'shrub',
+              waterDate: 1,
+              fertilizerDate: 7,
+              mistingDate: 5,
+            })
+            .then((postedPlantResource) => {
+              resultMock.postedPlantResource = postedPlantResource.body;
+              return superagent.delete(`${apiURL}/bloomiogarden/${postedPlantResource.body._id}`)
+                .set('Authorization', `Bearer ${resultMock.token}`)
+                .then((response) => {
+                  expect(response.status).toEqual(204);
+                });
+            });
+        });
+    });
+    test('DELETE - should return a 401 status code if user did not have admin access.', () => {
+      const resultMock = {};
+      return createAdminMock()
+        .then((responseMock) => {
+          const { token } = responseMock.accountSetMock;
+          resultMock.token = token;
+          return superagent.post(`${apiURL}/bloomiogarden`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+              commonName: 'Coriander',
+              scientificName: 'Coriandrum sativum',
+              groupType: 'herb',
+              waterDate: 3,
+              fertilizerDate: 30,
+              mistingDate: 30,
+            })
+            .then((postedPlantResource) => {
+              resultMock.postedPlantResource = postedPlantResource.body;
+              return createProfileMock()
+                .then((secondResponseMock) => {
+                  const { userToken } = secondResponseMock.accountSetMock;
+                  return superagent.delete(`${apiURL}/bloomiogarden/${resultMock.postedPlantResource._id}`)
+                    .set('Authorization', `Bearer ${userToken}`)
+                    .then(Promise.reject)
+                    .catch((error) => {
+                      expect(error.status).toEqual(401);
+                    });
+                });
+            });
+        });
+    });
+    test('DELETE - should return a 404 status code for a bad address.', () => {
+      return createAdminMock()
+        .then((responseMock) => {
+          const { token } = responseMock.accountSetMock;
+          return superagent.post(`${apiURL}/bloomiogardebadaddress`)
+            .set('Authorization', `Bearer ${token}`);
+        })
+        .then(Promise.reject)
+        .catch((error) => {
+          expect(error.status).toEqual(404);
         });
     });
   });
